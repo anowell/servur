26d5e9fb636b887db3e5a8a4092c92deec6669ef
Anthony Nowell <anowell@gmail.com>
2015-02-14 14:37:31 -0800

    Stream runner's stdout through arest's stdout

--- src/controller.rs
+++ src/controller.rs
@@ -2,8 +2,9 @@ extern crate http;
 extern crate libc;
 
 use ::{Application, ArestError};
-use std::old_io::process::{Process, Command};
 use nickel::{Request, Response};
+// use std::thread::Thread;
+use std::old_io::process::{Process, Command, ProcessExit};
 
 pub fn get_hello(_: &Request, response: &mut Response) {
     response.send("Alive and well");
@@ -20,12 +21,12 @@ pub fn get_status(_: &Request, response: &mut Response, app: &Application) {
 pub fn post_signal(request: &Request, response: &mut Response, app: &Application) {
     let signal_param = request.param("signal");
     let signal = match signal_param {
-        // SIGQUIT: quit and perform core dump
-        "sigquit" => libc::consts::os::posix88::SIGQUIT as isize,
-        // SIGKILL: immediately kill process (i.e. kill -9)
-        "sigkill" => libc::SIGKILL as isize,
         // SIGTERM: terminate process (i.e. default kill)
-        "sigterm" => libc::SIGTERM as isize,
+        "term" => libc::SIGTERM as isize,
+        // SIGKILL: immediately kill process (i.e. kill -9)
+        "kill" => libc::SIGKILL as isize,
+        // SIGQUIT: quit and perform core dump
+        "quit" => libc::consts::os::posix88::SIGQUIT as isize,
         // Error for all other signals
         unknown_signal => {
             println!("post_signal: invalid signal: {}", unknown_signal);
@@ -47,29 +48,74 @@ pub fn post_signal(request: &Request, response: &mut Response, app: &Application
     };
 }
 
+const PIPE_BUF_SIZE: usize = 4096; // max bytes read per interval
+const PIPE_INTERVAL: u64 = 500; // ms between checking stdout
+
+fn pipe_child_output(child: &mut Process) {
+      let mut buf = [0u8; PIPE_BUF_SIZE];
+      child.stdout.as_mut().unwrap().read(&mut buf).ok();
+      // TODO: Pipe both stdout and stderr:
+      //       child.stdout | arest.stdout
+      //       child.stderr | arest.sterr
+      print!("{}", String::from_utf8_lossy(&buf));
+}
+
 pub fn post_data(request: &Request, response: &mut Response, app: &Application) {
     let runner = &*(app.runner);
+    // TODO: fail if busy running another process
 
-    let spawn_runner = || {
-        let mut process = try!(Command::new(runner).spawn());
-        app.set_pid(Some(process.id()));
-        let mut child_stdin = process.stdin.take().unwrap();
+    let spawn_runner = || -> Result<Process, ArestError> {
+        // Start child process
+        let mut child = try!(Command::new(runner).spawn());
+        println!("Started {} with pid: {}", runner, child.id());
+        app.set_pid(Some(child.id()));
+
+        // Pipe request body to child stdin
+        let mut child_stdin = child.stdin.take().unwrap();
         try!(child_stdin.write_all(&request.origin.body));
-        Ok(process)
+        Ok(child)
     };
 
-    let process_result: Result<Process, ArestError> = spawn_runner();
-    let message = match process_result {
-        Ok(mut process) =>  {
-            let output = process.stdout.as_mut().unwrap().read_to_string().ok().unwrap();
-            format!("{} output: {}!", runner, output)
-        },
+    match spawn_runner() {
         Err(why) => {
             response.status_code(http::status::InternalServerError);
-            format!("Could not read {} stdout: {:?}", runner, why)
-        }
+            let message = format!("Could not read {} stdout: {:?}", runner, why);
+            println!("{}", message);
+            response.send(message);
+        },
+        Ok(mut child) =>  {
+            // Thread::spawn(|| {
+                loop {
+                    // This is how we stream the stdout pipe: a chunk every PIPE_INTERVAL
+                    child.set_timeout(Some(PIPE_INTERVAL));
+                    match child.wait() {
+                        Err(..) => {
+                            // Err is generally TimedOut or EndOfFile
+                            //   but regardless, we pipe until a ProcessExit happens
+                            pipe_child_output(&mut child);
+                        }
+                        Ok(exit_condition) => {
+                            pipe_child_output(&mut child);
+                            match exit_condition {
+                                ProcessExit::ExitStatus(0) => println!("Finished without errors"),
+                                ProcessExit::ExitStatus(a) => println!("Finished with error number: {}", a),
+                                ProcessExit::ExitSignal(a) => println!("Terminated by signal number: {}", a),
+                            }
+                            // TODO: update Application state
+                            return;
+                        }
+                    }
+                }
+            // });
+
+
+        },
     };
 
-    println!("{}", message);
-    response.send(message);
+
+
+
+
+
+
 }
